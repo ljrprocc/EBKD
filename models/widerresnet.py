87daae8d30a1279d5a88e91bec62b111536d9b15
ljrprocc <jrlees@zju.edu.cn>
2021-08-10 13:42:04 +0800

    update 0810

--- models/widerresnet.py
+++ models/widerresnet.py
@@ -15,6 +15,7 @@
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
+from torch.nn.utils import spectral_norm
 # import norms
 import numpy as np
 
@@ -44,14 +45,21 @@ class wide_basic(nn.Module):
         self.lrelu = nn.LeakyReLU(leak)
         self.bn1 = get_norm(in_planes, norm)
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, padding=1, bias=True)
+        if norm == 'spectral':
+            self.conv1 = spectral_norm(self.conv1)
         self.dropout = Identity() if dropout_rate == 0.0 else nn.Dropout(p=dropout_rate)
         self.bn2 = get_norm(planes, norm)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+        if norm == 'spectral':
+            self.conv2 = spectral_norm(self.conv2)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
+            short_conv = nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True)
+            if norm == 'spectral':
+                short_conv = spectral_norm(short_conv)
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True),
+                short_conv,
             )
 
     def forward(self, x):
@@ -64,7 +72,7 @@ class wide_basic(nn.Module):
 
 
 def get_norm(n_filters, norm):
-    if norm == 'none':
+    if norm == 'none' or norm == 'spectral':
         return Identity()
     elif norm == "batch":
         return nn.BatchNorm2d(n_filters, momentum=0.9)
@@ -94,6 +102,8 @@ class Wide_ResNet(nn.Module):
         nStages = [16, 16*k, 32*k, 64*k]
 
         self.conv1 = conv3x3(input_channels, nStages[0])
+        if norm == 'spectral':
+            self.conv1 = spectral_norm(self.conv1)
         self.layer1 = self._wide_layer(wide_basic, nStages[1], n, dropout_rate, stride=1)
         self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=2)
         self.layer3 = self._wide_layer(wide_basic, nStages[3], n, dropout_rate, stride=2)
